e9d356d68704e645580fb946cf8e6c16c6758911
Shivang002 <123627220+Shivang002@users.noreply.github.com>
2023-12-06 22:44:54 +0000

    update

---
+++ basic/3.datatype.js
@@ -0,0 +1,11 @@
+"use strict" // defines that we are using newer version of the js
+
+// alert("") // used for the pop up message in browser not in node 
+
+console.log(3+3) //code readility should be high
+console.log("Shivang")
+
+// primitive
+// int , string,boolean, undefined ,null
+
+//object
---
+++ basic/var.js
@@ -0,0 +1,16 @@
+const accountId=1234
+let accountEmail="asndn"
+var accountPass=" "
+accountCity="jaipur"
+
+//  accountId=1
+//  console.log(accountId);
+// to print multiple attribute in form of table hum log use karenge
+console.table([accountId,accountEmail,accountPass,accountCity])
+ accountEmail="asndn"
+ accountPass=" "
+accountCity="jaipur"
+let accountState;
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+//for not defined variables for eg accountState the js will print undefined
