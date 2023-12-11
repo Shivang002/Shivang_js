c4fcd2fe37cce7444edc8877b4179f94c8c7f8b8
Shivang002 <123627220+Shivang002@users.noreply.github.com>
2023-12-11 19:48:50 +0000

    upd

---
+++ basic/9.numsMath.js
@@ -0,0 +1,26 @@
+const num=90
+console.log(num);
+const num1=new Number(90)
+console.log(num1);
+
+//number -> string
+console.log(num.toString());
+console.log(num1.toString().length);
+
+//to fixed operation
+console.log(num.toFixed(2));
+
+//precision operator
+const other=123.456
+console.log(other.toPrecision(4)); //isme jo bracket anadar value pass kia vo hume ye batayaga ki starting se kitne digits ke baad precise /round off karna h
+
+
+//++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// uses maths operation like
+//max
+//min
+//abs\
+//pi
+//round,ciel,floor and many more
+
